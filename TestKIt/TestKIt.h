41fb8f1b42e68ac17f13c4457994c47bded8f2b4
hwzss <1833361588@qq.com>
2017-11-02 17:21:20 +0800

    首次提交

---
+++ TestKIt/TestKIt.h
@@ -0,0 +1,19 @@
+//
+//  TestKIt.h
+//  TestKIt
+//
+//  Created by qwkj on 2017/11/2.
+//  Copyright © 2017年 qwkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestKIt.
+FOUNDATION_EXPORT double TestKItVersionNumber;
+
+//! Project version string for TestKIt.
+FOUNDATION_EXPORT const unsigned char TestKItVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestKIt/PublicHeader.h>
+
+
